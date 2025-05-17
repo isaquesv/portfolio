0ce802a4e809a613013a8bf9fe5a39f102fba0ac
isaquesv <isaque.s.venancio43@gmail.com>
2025-05-17 00:02:05 -0300

    Realizando pequenas alterações no código

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,4 +1,4 @@
 const CURRENT_YEAR = new Date().getFullYear();
 const FOOTER_LINK = document.querySelector("footer a");
 
-FOOTER_LINK.innerHTML = "© 2025–" + CURRENT_YEAR + " <b>isaquesv</b>. Todos os direitos reservados.";
+FOOTER_LINK.innerHTML = "© 2025–" + CURRENT_YEAR + " isaquesv. Todos os direitos reservados.";
